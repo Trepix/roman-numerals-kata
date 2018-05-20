c2c35ab49e8de33918a445abba5645df38b6537d
trepix <pau.trepat.segura@gmail.com>
2018-05-20 12:44:19 +0200

    Solve Step 1

--- src/main/java/io/trepixxx/romannumerals/DecimalNumeral.java
+++ src/main/java/io/trepixxx/romannumerals/DecimalNumeral.java
@@ -1,27 +1,34 @@
 package io.trepixxx.romannumerals;
 
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
+import java.math.BigInteger;
+
 @EqualsAndHashCode
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 class DecimalNumeral {
 
-    private final Integer value;
+    private final BigInteger value;
+
+    DecimalNumeral(Integer number) {
+        this.value = BigInteger.valueOf(number);
+    }
 
     static DecimalNumeral zero() {
         return new DecimalNumeral(0);
     }
 
     boolean isMinorThan(DecimalNumeral decimalNumeral) {
-        return this.value < decimalNumeral.value;
+        return this.value.compareTo(decimalNumeral.value) < 0;
     }
 
     DecimalNumeral add(DecimalNumeral decimalNumeral) {
-        return new DecimalNumeral(this.value + decimalNumeral.value);
+        return new DecimalNumeral(this.value.add(decimalNumeral.value));
     }
 
     DecimalNumeral subtract(DecimalNumeral decimalNumeral) {
-        return new DecimalNumeral(this.value - decimalNumeral.value);
+        return new DecimalNumeral(this.value.subtract(decimalNumeral.value));
     }
 }
--- src/main/java/io/trepixxx/romannumerals/RomanNumeralSymbol.java
+++ src/main/java/io/trepixxx/romannumerals/RomanNumeralSymbol.java
@@ -5,26 +5,29 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 
-
-@Getter
 @RequiredArgsConstructor
 enum RomanNumeralSymbol {
-    I('I', new DecimalNumeral(1)),
-    V('V', new DecimalNumeral(5)),
-    X('X', new DecimalNumeral(10)),
-    L('L', new DecimalNumeral(50)),
-    C('C', new DecimalNumeral(100)),
-    D('D', new DecimalNumeral(500)),
-    M('M', new DecimalNumeral(1000));
+    I("I", new DecimalNumeral(1)),
+    V("V", new DecimalNumeral(5)),
+    X("X", new DecimalNumeral(10)),
+    L("L", new DecimalNumeral(50)),
+    C("C", new DecimalNumeral(100)),
+    D("D", new DecimalNumeral(500)),
+    M("M", new DecimalNumeral(1000));
 
-    private final Character symbol;
+    private final String symbol;
+    @Getter
     private final DecimalNumeral decimalNumeral;
 
     static RomanNumeralSymbol of(Character character) {
         return Arrays.stream(values())
-                .filter(romanNumeralSymbol -> romanNumeralSymbol.symbol.equals(character))
+                .filter(romanNumeralSymbol -> romanNumeralSymbol.getSymbol().equals(character))
                 .findAny()
                 .orElseThrow(() -> new InvalidRomanNumeralSymbolException(character));
+    }
+
+    Character getSymbol() {
+        return symbol.charAt(0);
     }
 
     boolean isMinorThan(RomanNumeralSymbol romanNumeralSymbol) {
